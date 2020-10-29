900976d0228ac0d7ddd2dc89b16fda54e513bdae
mappingmilwaukee <60715235+mappingmilwaukee@users.noreply.github.com>
2020-10-29 00:31:52 -0500

    Add files via upload

---
+++ data/STF_9.js
@@ -0,0 +1,8 @@
+var json_STF_9 = {
+"type": "FeatureCollection",
+"name": "STF_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "currname": "1828 St Francis", "formname": null, "fulladd": "1828 E St Francis Av", "geolat": 42.979345, "geolong": -87.886205, "formadd": null, "city": "St Francis", "state": "WI", "ZIP": 53235.0, "bdate": "TBD", "rdate": "extant", "razedYN": "n", "plan": null, "styles": null, "archfirm": null, "archts": null, "bldrs": null, "owners": null, "floors": null, "units": null, "cost": null, "materials": "brick", "fallout": null, "Notes": null, "links1": null, "links2": null, "links3": null, "links4": null, "links5": null, "links6": null, "links7": null, "links8": null, "WHSAHI": null, "OldMKE": null, "MPLHPA": null, "MPLMAI": null, "MPLWAA": null, "MCHSLBC": null, "type": "apartment building", "citydiv": "St Francis" }, "geometry": { "type": "Point", "coordinates": [ -87.886205, 42.979345 ] } }
+]
+}
